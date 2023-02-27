69119b910d530f2363505eae55679abafe76a66e
Vinod <mannavasai999@gmail.com>
2023-02-27 11:45:56 -0600

    Methods program

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main(String[] args){
+    System.out.println("Hello This is main");
+    anything();
+}
+public static void anything(){
+    System.out.println("Hello, This is anything Method");
+}
+}
